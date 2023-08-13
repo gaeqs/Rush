a79b57d6058c3bd4cb54d63c931b41de61a6ae28
Gael Rial Costas <gael.rial.costas@gmail.com>
2023-08-13 04:34:24 +0200

    Start matrix benchmarks.

---
+++ tests/matrix_benchmark.cpp
@@ -0,0 +1,37 @@
+//
+// Created by gaelr on 13/08/23.
+//
+
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/benchmark/catch_benchmark.hpp>
+#include <iostream>
+#include <random>
+#include <rush/rush.h>
+
+using V1 = rush::Vec<1, int>;
+using V2 = rush::Vec<2, int>;
+using V3 = rush::Vec<3, int>;
+using V5 = rush::Vec<5, int>;
+using Mat2 = rush::Mat<2, 2, int>;
+using Mat3 = rush::Mat<3, 3, int>;
+using Mat4 = rush::Mat<4, 4, int>;
+using Mat2f = rush::Mat<2, 2, float>;
+using Mat3f = rush::Mat<3, 3, float>;
+using Mat4f = rush::Mat<4, 4, float>;
+
+TEST_CASE("Big matrix multiplication (double)", "[!benchmark]") {
+    BENCHMARK_ADVANCED("100")(Catch::Benchmark::Chronometer meter) {
+            std::random_device rd;
+            std::mt19937 gen(rd());
+            std::uniform_real_distribution<> distr(0.0, 1.0);
+
+            rush::Mat<100, 100, double> a([&](size_t c, size_t r) {
+                return distr(gen);
+            });
+            rush::Mat<100, 100, double> b([&](size_t c, size_t r) {
+                return distr(gen);
+            });
+
+            meter.measure([a, b]() { return a * b; });
+        };
+}
