8ec45536d7a3c45d1c22b3975c8070f42d1868d7
Gael Rial Costas <gael.rial.costas@gmail.com>
2023-09-07 08:34:13 +0200

    Improve quaternion.

--- src/rush/quaternion/quat_base.h
+++ src/rush/quaternion/quat_base.h
@@ -13,9 +13,7 @@ namespace rush {
     template<typename Type>
     struct Quat {
 
-        using Self = Quat<Type>;
-
-        Type d, a, b, c;
+        Type s, x, y, z;
 
         Quat();
 
@@ -23,21 +21,49 @@ namespace rush {
 
         Quat(Type angle, Vec<3, Type> direction);
 
-        inline Type squaredLength() requires HasAdd<Type> && HasMul<Type>;
+        // REGION OPERATORS
 
-        inline Type length() requires HasSquaredRoot<Type>;
+        // UNARY
 
-        inline Quat normalize() requires HasDiv<Type>;
+        inline Type squaredLength() const requires HasAdd<Type> && HasMul<Type>;
 
-        inline void normalized() requires HasDiv<Type>;
+        inline Type length() const requires HasSquaredRoot<Type>;
 
-        // REGION STATIC
+        inline Quat normalized() const requires HasDiv<Type>;
 
-        inline static Self angleAxis(Type angle, const Vec<3, Type>& axis);
+        inline void normalize() requires HasDiv<Type>;
 
-        inline static Self euler(const Vec<3, Type>& angles);
+        inline Quat conjugated() const;
 
-        inline static Self
+        inline void conjugate();
+
+        inline Quat inverse() const;
+
+        inline void invert();
+
+        // QUAT - SCALAR
+
+        inline Quat operator+(const Type& o) const requires HasAdd<Type>;
+        inline Quat operator-(const Type& o) const requires HasAdd<Type>;
+        inline Quat operator*(const Type& o) const requires HasAdd<Type>;
+        inline Quat operator/(const Type& o) const requires HasAdd<Type>;
+
+        // QUAT - QUAT
+
+        inline Quat operator+(const Quat& o) const requires HasAdd<Type>;
+        inline Quat operator-(const Quat& o) const requires HasSub<Type>;
+        inline Quat operator*(const Quat& o) const requires
+        HasAdd<Type> && HasSub<Type> && HasMul<Type>;
+
+        // ENDREGION
+
+        // REGION STATIC CREATION
+
+        inline static Quat angleAxis(Type angle, const Vec<3, Type>& axis);
+
+        inline static Quat euler(const Vec<3, Type>& angles);
+
+        inline static Quat
         fromTo(const Vec<3, Type>& from, const Vec<3, Type>& to);
 
         // ENDREGION
--- src/rush/quaternion/quat_impl.h
+++ src/rush/quaternion/quat_impl.h
@@ -10,12 +10,12 @@
 namespace rush {
 
     template<typename Type>
-    Quat<Type>::Quat() : d(Type(0)), a(Type(1)), b(Type(0)), c(Type(0)) {
+    Quat<Type>::Quat() : s(Type(0)), x(Type(1)), y(Type(0)), z(Type(0)) {
     }
 
     template<typename Type>
     Quat<Type>::Quat(Type d_, Type a_, Type b_, Type c_) :
-            d(d_), a(a_), b(b_), c(c_) {
+            s(d_), x(a_), y(b_), z(c_) {
     }
 
     template<typename Type>
@@ -24,32 +24,107 @@ namespace rush {
     }
 
     template<typename Type>
-    Type Quat<Type>::squaredLength() requires HasAdd<Type> && HasMul<Type> {
-        return d * d + a * a + b * b + c * c;
+    Type
+    Quat<Type>::squaredLength() const requires HasAdd<Type> && HasMul<Type> {
+        return s * s + x * x + y * y + z * z;
     }
 
     template<typename Type>
-    Type Quat<Type>::length() requires HasSquaredRoot<Type> {
+    Type Quat<Type>::length() const requires HasSquaredRoot<Type> {
         return std::sqrt(squaredLength());
     }
 
     template<typename Type>
-    Quat<Type> Quat<Type>::normalize() requires HasDiv<Type> {
+    Quat<Type> Quat<Type>::normalized() const requires HasDiv<Type> {
         Type l = length();
-        return {d / l, a / l, b / l, c / l};
+        return {s / l, x / l, y / l, z / l};
     }
 
     template<typename Type>
-    void Quat<Type>::normalized() requires HasDiv<Type> {
+    void Quat<Type>::normalize() requires HasDiv<Type> {
         Type l = length();
-        d /= l;
-        a /= l;
-        b /= l;
-        c /= l;
+        s /= l;
+        x /= l;
+        y /= l;
+        z /= l;
     }
 
     template<typename Type>
-    Quat<Type>::Self
+    Quat<Type> Quat<Type>::conjugated() const {
+        return {s, -x, -y, -z};
+    }
+
+    template<typename Type>
+    void Quat<Type>::conjugate() {
+        x = -x;
+        y = -y;
+        z = -z;
+    }
+
+    template<typename Type>
+    Quat<Type> Quat<Type>::inverse() const {
+        return conjugated() / squaredLength();
+    }
+
+    template<typename Type>
+    void Quat<Type>::invert() {
+        Type sl = squaredLength();
+        s = s / sl;
+        x = -x / sl;
+        y = -y / sl;
+        z = -z / sl;
+    }
+
+    template<typename Type>
+    Quat<Type>
+    Quat<Type>::operator+(const Type& o) const requires HasAdd<Type> {
+        return {s + o, x + o, y + o, z + o};
+    }
+
+    template<typename Type>
+    Quat<Type>
+    Quat<Type>::operator-(const Type& o) const requires HasAdd<Type> {
+        return {s - o, x - o, y - o, z - o};
+    }
+
+    template<typename Type>
+    Quat<Type>
+    Quat<Type>::operator*(const Type& o) const requires HasAdd<Type> {
+        return {s * o, x * o, y * o, z * o};
+    }
+
+    template<typename Type>
+    Quat<Type>
+    Quat<Type>::operator/(const Type& o) const requires HasAdd<Type> {
+        return {s / o, x / o, y / o, z / o};
+    }
+
+
+    template<typename Type>
+    Quat<Type>
+    Quat<Type>::operator+(const Quat& o) const requires HasAdd<Type> {
+        return {s + o.s, x + o.x, y + o.y, z + o.z};
+    }
+
+    template<typename Type>
+    Quat<Type>
+    Quat<Type>::operator-(const Quat& o) const requires HasSub<Type> {
+        return {s - o.s, x - o.x, y - o.y, z - o.z};
+    }
+
+    template<typename Type>
+    Quat<Type> Quat<Type>::operator*(const Quat& o) const requires
+    HasAdd<Type> && HasSub<Type> && HasMul<Type> {
+        Quat<Type> result;
+        result.s = s * o.s - x * o.x - y * o.y - z * o.z;
+        result.x = s * o.x + x * o.s + y * o.z - z * o.y;
+        result.y = s * o.y - x * o.z + y * o.s + z * o.x;
+        result.z = s * o.z + x * o.y - y * o.x + z * o.s;
+        return result;
+    }
+
+    template<typename Type>
+    Quat<Type>
     Quat<Type>::angleAxis(Type angle, const Vec<3, Type>& axis) {
         Type sin = std::sin(angle / Type(2));
         Type d = std::cos(angle / Type(2));
@@ -60,7 +135,7 @@ namespace rush {
     }
 
     template<typename Type>
-    Quat<Type>::Self Quat<Type>::euler(const Vec<3, Type>& angles) {
+    Quat<Type> Quat<Type>::euler(const Vec<3, Type>& angles) {
         Vec<3, Type> c = rush::cos(angles);
         Vec<3, Type> s = rush::sin(angles);
 
@@ -73,7 +148,7 @@ namespace rush {
     }
 
     template<typename Type>
-    Quat<Type>::Self
+    Quat<Type>
     Quat<Type>::fromTo(const Vec<3, Type>& from, const Vec<3, Type>& to) {
         Type uv = std::sqrt(from.squaredLength() * to.squaredLength());
         Type real = uv + from % to;
