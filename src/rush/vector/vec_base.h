faed86a3a18b6ffd8313543241326f5b93c288b2
Gael Rial Costas <gael.rial.costas@gmail.com>
2023-11-21 18:06:50 +0100

    Finish documentation for vector struct.

--- src/rush/vector/vec_base.h
+++ src/rush/vector/vec_base.h
@@ -5,11 +5,7 @@
 #ifndef RUSH_VEC_BASE_H
 #define RUSH_VEC_BASE_H
 
-#include <utility>
 #include <functional>
-#include <algorithm>
-#include <string>
-#include <iterator>
 #include <array>
 
 #include <rush/concepts.h>
@@ -23,15 +19,22 @@
 #endif
 
 namespace rush {
-
+    /**
+     * Represents a mathematical vector.
+     * A vector can be defined as an array of values used for mathematical
+     * operations.
+     *
+     * @tparam Size the size of the vector.
+     * @tparam Type the type of the vector.
+     * @tparam Allocator the allocators used to store the data of the vector.
+     */
     template<size_t Size,
-            typename Type,
-            typename Allocator = StaticAllocator> requires (Size > 0)
+        typename Type,
+        typename Allocator = StaticAllocator> requires (Size > 0)
     struct Vec {
-
         using Storage = Allocator;
 
-        Allocator::template AllocatedData<Size, Type> data;
+        typename Allocator::template AllocatedData<Size, Type> data;
 
         /**
          * Creates a new empty vector.
@@ -55,8 +58,8 @@ namespace rush {
          * @param list the values.
          */
         template<typename... T>
-        requires std::is_convertible_v<std::common_type_t<T...>, Type> &&
-                 (Size > 1 && sizeof...(T) == Size)
+            requires std::is_convertible_v<std::common_type_t<T...>, Type> &&
+                     (Size > 1 && sizeof...(T) == Size)
         Vec(T... list);
 
         /**
@@ -69,7 +72,7 @@ namespace rush {
          * @param other the given vector.
          */
         template<size_t OSize, typename OAlloc>
-        requires(Size < OSize)
+            requires(Size < OSize)
         Vec(const Vec<OSize, Type, OAlloc>& other);
 
         /**
@@ -83,8 +86,8 @@ namespace rush {
          * @param list the given list.
          */
         template<size_t OSize, typename OAlloc, typename... T>
-        requires std::is_convertible_v<std::common_type_t<T...>, Type> &&
-                 (sizeof...(T) + OSize == Size)
+            requires std::is_convertible_v<std::common_type_t<T...>, Type> &&
+                     (sizeof...(T) + OSize == Size)
         Vec(const Vec<OSize, Type, OAlloc>& other, T... list);
 
         /**
@@ -120,7 +123,7 @@ namespace rush {
          * to this pointer.
          * @return the pointer.
          */
-        [[nodiscard]] inline Type* toPointer();
+        [[nodiscard]] Type* toPointer();
 
         /**
          * Returns a pointer to the first element of this
@@ -129,7 +132,7 @@ namespace rush {
          * to this pointer.
          * @return the pointer.
          */
-        [[nodiscard]] inline const Type* toPointer() const;
+        [[nodiscard]] const Type* toPointer() const;
 
         /**
          * Returns the size of this vector.
@@ -139,25 +142,69 @@ namespace rush {
 
         // REGION ACCESSORS
 
-        inline Type& x();
+        /**
+         * Returns the first value of this vector.
+         * @return the first value.
+         */
+        Type& x();
 
-        inline Type& y() requires (Size >= 2);
+        /**
+         * Returns the second value of this vector.
+         * @return the second value.
+         */
+        Type& y() requires (Size >= 2);
 
-        inline Type& z() requires (Size >= 3);
+        /**
+         * Returns the third value of this vector.
+         * @return the third value.
+         */
+        Type& z() requires (Size >= 3);
 
-        inline Type& w() requires (Size >= 4);
+        /**
+         * Returns the fourth value of this vector.
+         * @return the fourth value.
+         */
+        Type& w() requires (Size >= 4);
 
-        inline const Type& x() const;
+        /**
+         * Returns the first value of this vector.
+         * @return the first value.
+         */
+        const Type& x() const;
 
-        inline const Type& y() const requires (Size >= 2);
+        /**
+         * Returns the second value of this vector.
+         * @return the second value.
+         */
+        const Type& y() const requires (Size >= 2);
 
-        inline const Type& z() const requires (Size >= 3);
+        /**
+         * Returns the third value of this vector.
+         * @return the third value.
+         */
+        const Type& z() const requires (Size >= 3);
 
-        inline const Type& w() const requires (Size >= 4);
+        /**
+         * Returns the fourth value of this vector.
+         * @return the fourth value.
+         */
+        const Type& w() const requires (Size >= 4);
 
-        inline Type& operator[](size_t index);
+        /**
+         * Returns the value at the given index.
+         * This method doesn't check for overflows.
+         * @param index the index.
+         * @return the value at the given index.
+         */
+        Type& operator[](size_t index);
 
-        inline const Type& operator[](size_t index) const;
+        /**
+         * Returns the value at the given index.
+         * This method doesn't check for overflows.
+         * @param index the index.
+         * @return the value at the given index.
+         */
+        const Type& operator[](size_t index) const;
 
         /**
          * Returns a vector referencing the given indices
@@ -165,8 +212,6 @@ namespace rush {
          * <p>
          * This method can be used to swizzle the values
          * of the vector.
-         *
-         * @example
          *
          * rush::Vec<3, float> a = {2, 4, 6};
          * <p>
@@ -182,9 +227,9 @@ namespace rush {
          * @return the vector reference.
          */
         template<typename... Ts>
-        requires std::is_convertible_v<std::common_type_t<Ts...>, size_t>
+            requires std::is_convertible_v<std::common_type_t<Ts...>, size_t>
         VecRef<sizeof...(Ts), Type>
-        operator()(Ts&& ... indices);
+        operator()(Ts&&... indices);
 
         /**
          * Returns a vector with the data at the given indices
@@ -209,9 +254,9 @@ namespace rush {
          * @return the new vector.
          */
         template<typename... Ts, typename OAlloc = Allocator>
-        requires std::is_convertible_v<std::common_type_t<Ts...>, size_t>
+            requires std::is_convertible_v<std::common_type_t<Ts...>, size_t>
         Vec<sizeof...(Ts), Type, OAlloc>
-        operator()(Ts&& ... indices) const;
+        operator()(Ts&&... indices) const;
 
         // ENDREGION
 
@@ -226,7 +271,7 @@ namespace rush {
          *
          * @return the squared length.
          */
-        inline Type squaredLength() const;
+        Type squaredLength() const;
 
         /**
          * Returns the length (or modulus) of this vector.
@@ -236,8 +281,8 @@ namespace rush {
          * @return the length.
          */
         template<typename Return = Type>
-        inline Return length() const requires (
-        std::is_convertible_v<Type, Return> && HasSquaredRoot<Type>);
+        Return length() const requires (
+            std::is_convertible_v<Type, Return> && HasSquaredRoot<Type>);
 
         /**
          * Returns the inverse of the length (or modulus) of this vector.
@@ -258,7 +303,7 @@ namespace rush {
          */
         template<typename Return = Type, Algorithm Algorithm = Algorithm()>
         Return inverseLength() const requires (
-        std::is_convertible_v<Type, Return> && HasSquaredRoot<Type>);
+            std::is_convertible_v<Type, Return> && HasSquaredRoot<Type>);
 
         /**
          * Returns a normalized version of this vector.
@@ -276,196 +321,236 @@ namespace rush {
          *
          */
         template<typename Return = Type, Algorithm Algorithm = Algorithm(),
-                typename OAlloc = Allocator>
+            typename OAlloc = Allocator>
         Vec<Size, Return, OAlloc> normalized() const requires HasMul<Return>;
 
+        /**
+         * Returns a copy of this vector with all its elements
+         * casted to the given type.
+         * @tparam To the type.
+         * @tparam OAlloc the allocator used on the new vector.
+         * @return the new vector.
+         */
         template<typename To, typename OAlloc = Allocator>
         Vec<Size, To, OAlloc> cast() const;
 
+        /**
+         * Returns an std::array containing the elements
+         * of this vector.
+         *
+         * \return the array.
+         */
         std::array<Type, Size> toArray() const;
 
-        inline Vec& operator+();
+        Vec& operator+();
 
-        inline const Vec& operator+() const;
+        const Vec& operator+() const;
 
-        inline Vec operator-() const requires HasSub<Type>;
+        Vec operator-() const requires HasSub<Type>;
 
         // ASSIGN VECTOR - SCALE
 
-        inline Vec& operator+=(const Type& s) requires HasAdd<Type>;
+        Vec& operator+=(const Type& s) requires HasAdd<Type>;
 
-        inline Vec& operator-=(const Type& s) requires HasSub<Type>;
+        Vec& operator-=(const Type& s) requires HasSub<Type>;
 
-        inline Vec& operator*=(const Type& s) requires HasMul<Type>;
+        Vec& operator*=(const Type& s) requires HasMul<Type>;
 
-        inline Vec& operator/=(const Type& s) requires HasDiv<Type>;
+        Vec& operator/=(const Type& s) requires HasDiv<Type>;
 
-        inline Vec& operator<<=(const Type& s) requires HasShl<Type>;
+        Vec& operator<<=(const Type& s) requires HasShl<Type>;
 
-        inline Vec& operator>>=(const Type& s) requires HasShr<Type>;
+        Vec& operator>>=(const Type& s) requires HasShr<Type>;
 
-        inline Vec& operator&=(const Type& s) requires HasBitAnd<Type>;
+        Vec& operator&=(const Type& s) requires HasBitAnd<Type>;
 
-        inline Vec& operator|=(const Type& s) requires HasBitOr<Type>;
+        Vec& operator|=(const Type& s) requires HasBitOr<Type>;
 
-        inline Vec& operator^=(const Type& s) requires HasBitXor<Type>;
+        Vec& operator^=(const Type& s) requires HasBitXor<Type>;
 
         // ASSIGN VECTOR - VECTOR
 
         template<typename OAlloc>
-        inline Vec&
+        Vec&
         operator+=(const Vec<Size, Type, OAlloc>& o) requires HasAdd<Type>;
 
         template<typename OAlloc>
-        inline Vec&
+        Vec&
         operator-=(const Vec<Size, Type, OAlloc>& o) requires HasSub<Type>;
 
         template<typename OAlloc>
-        inline Vec&
+        Vec&
         operator*=(const Vec<Size, Type, OAlloc>& o) requires HasMul<Type>;
 
         template<typename OAlloc>
-        inline Vec&
+        Vec&
         operator/=(const Vec<Size, Type, OAlloc>& o) requires HasDiv<Type>;
 
         template<typename OAlloc>
-        inline Vec&
+        Vec&
         operator<<=(const Vec<Size, Type, OAlloc>& o) requires HasShl<Type>;
 
         template<typename OAlloc>
-        inline Vec&
+        Vec&
         operator>>=(const Vec<Size, Type, OAlloc>& o) requires HasShr<Type>;
 
         template<typename OAlloc>
-        inline Vec&
+        Vec&
         operator&=(const Vec<Size, Type, OAlloc>& o) requires HasBitAnd<Type>;
 
         template<typename OAlloc>
-        inline Vec&
+        Vec&
         operator|=(const Vec<Size, Type, OAlloc>& o) requires HasBitOr<Type>;
 
         template<typename OAlloc>
-        inline Vec&
+        Vec&
         operator^=(const Vec<Size, Type, OAlloc>& o) requires HasBitXor<Type>;
 
         // VECTOR - SCALE
 
-        inline Vec operator+(const Type& s) const requires HasAdd<Type>;
+        Vec operator+(const Type& s) const requires HasAdd<Type>;
 
-        inline Vec operator-(const Type& s) const requires HasSub<Type>;
+        Vec operator-(const Type& s) const requires HasSub<Type>;
 
-        inline Vec operator*(const Type& s) const requires HasMul<Type>;
+        Vec operator*(const Type& s) const requires HasMul<Type>;
 
-        inline Vec operator/(const Type& s) const requires HasDiv<Type>;
+        Vec operator/(const Type& s) const requires HasDiv<Type>;
 
-        inline Vec operator%(const Type& s) const requires HasMod<Type>;
+        Vec operator%(const Type& s) const requires HasMod<Type>;
 
-        inline Vec operator<<(const Type& s) const requires HasShl<Type>;
+        Vec operator<<(const Type& s) const requires HasShl<Type>;
 
-        inline Vec operator>>(const Type& s) const requires HasShr<Type>;
+        Vec operator>>(const Type& s) const requires HasShr<Type>;
 
-        inline Vec operator&(const Type& s) const requires HasBitAnd<Type>;
+        Vec operator&(const Type& s) const requires HasBitAnd<Type>;
 
-        inline Vec operator|(const Type& s) const requires HasBitOr<Type>;
+        Vec operator|(const Type& s) const requires HasBitOr<Type>;
 
-        inline Vec operator^(const Type& s) const requires HasBitXor<Type>;
+        Vec operator^(const Type& s) const requires HasBitXor<Type>;
 
-        inline Vec operator&&(const Type& s) const requires HasAnd<Type>;
+        Vec operator&&(const Type& s) const requires HasAnd<Type>;
 
-        inline Vec operator||(const Type& s) const requires HasOr<Type>;
+        Vec operator||(const Type& s) const requires HasOr<Type>;
 
         // VECTOR - VECTOR
 
+        /**
+         * Returns the angle created by this vector and the given
+         * vector.
+         * <p>
+         * You can select the precision of the algorithm.
+         * A high precision algorithm will provide a high precision
+         * result in exchange of more computational cost.
+         * A low precision algorithm will be faster, but it may provide
+         * poorer results.
+         * @tparam Return the result type.
+         * @tparam Algorithm the algorithm configuration data.
+         * @tparam OAlloc the allocator of the givem vector.
+         * @param other the given vector.
+         * @return the angle, casted to the result type.
+         */
         template<typename Return = Type, Algorithm Algorithm = Algorithm(),
-                typename OAlloc>
+            typename OAlloc>
         Return angle(const Vec<Size, Type, OAlloc>& other);
 
         template<typename OAlloc>
-        inline Vec operator+(const Vec<Size, Type, OAlloc>& other)
+        Vec operator+(const Vec<Size, Type, OAlloc>& other)
         const requires HasAdd<Type>;
 
         template<typename OAlloc>
-        inline Vec operator-(const Vec<Size, Type, OAlloc>& other)
+        Vec operator-(const Vec<Size, Type, OAlloc>& other)
         const requires HasSub<Type>;
 
         template<typename OAlloc>
-        inline Vec operator*(const Vec<Size, Type, OAlloc>& other)
+        Vec operator*(const Vec<Size, Type, OAlloc>& other)
         const requires HasMul<Type>;
 
         template<typename OAlloc>
-        inline Vec operator/(const Vec<Size, Type, OAlloc>& other)
+        Vec operator/(const Vec<Size, Type, OAlloc>& other)
         const requires HasDiv<Type>;
 
         template<typename OAlloc>
-        inline Type operator%(const Vec<Size, Type, OAlloc>& other)
+        Type operator%(const Vec<Size, Type, OAlloc>& other)
         const requires HasMod<Type>;
 
         template<typename OAlloc>
-        inline Vec operator<<(const Vec<Size, Type, OAlloc>& other)
+        Vec operator<<(const Vec<Size, Type, OAlloc>& other)
         const requires HasShl<Type>;
 
         template<typename OAlloc>
-        inline Vec operator>>(const Vec<Size, Type, OAlloc>& other)
+        Vec operator>>(const Vec<Size, Type, OAlloc>& other)
         const requires HasShr<Type>;
 
         template<typename OAlloc>
-        inline Vec operator&(const Vec<Size, Type, OAlloc>& other)
+        Vec operator&(const Vec<Size, Type, OAlloc>& other)
         const requires HasBitAnd<Type>;
 
         template<typename OAlloc>
-        inline Vec operator|(const Vec<Size, Type, OAlloc>& other)
+        Vec operator|(const Vec<Size, Type, OAlloc>& other)
         const requires HasBitOr<Type>;
 
         template<typename OAlloc>
-        inline Vec operator^(const Vec<Size, Type, OAlloc>& other)
+        Vec operator^(const Vec<Size, Type, OAlloc>& other)
         const requires HasBitXor<Type>;
 
         template<typename OAlloc>
-        inline Vec operator&&(const Vec<Size, Type, OAlloc>& other)
+        Vec operator&&(const Vec<Size, Type, OAlloc>& other)
         const requires HasAnd<Type>;
 
         template<typename OAlloc>
-        inline Vec operator||(const Vec<Size, Type, OAlloc>& other)
+        Vec operator||(const Vec<Size, Type, OAlloc>& other)
         const requires HasOr<Type>;
 
-        // DOT
+        /**
+         * Returns the dot product of this vector and the given one.
+         * Both vectors must have the same data type and size.
+         * @tparam OAlloc the allocator of the given vector.
+         * @param other the given vector.
+         * @return the result.
+         */
         template<typename OAlloc>
-        inline Type dot(const Vec<Size, Type, OAlloc>& other) const;
+        Type dot(const Vec<Size, Type, OAlloc>& other) const;
 
-        // CROSS
+        /**
+         * Returns the cross product of this vector and the given one.
+         * Both vectors must have the same data type and a size of 3.
+         * @tparam OAlloc the allocator of the given vector.
+         * @param other the given vector.
+         * @return the result.
+         */
         template<typename OAlloc>
-        inline Vec cross(const Vec<Size, Type, OAlloc>& other) const requires (
-        Size == 3 && HasAdd<Type> && HasMul<Type>);
+        Vec cross(const Vec<Size, Type, OAlloc>& other) const requires (
+            Size == 3 && HasAdd<Type> && HasMul<Type>);
 
         template<typename OAlloc>
-        inline bool operator==(const Vec<Size, Type, OAlloc>& other) const;
+        bool operator==(const Vec<Size, Type, OAlloc>& other) const;
 
         template<typename OAlloc>
-        inline bool operator!=(const Vec<Size, Type, OAlloc>& other) const;
+        bool operator!=(const Vec<Size, Type, OAlloc>& other) const;
 
-        inline bool operator==(const VecRef<Size, Type>& other) const;
+        bool operator==(const VecRef<Size, Type>& other) const;
 
-        inline bool operator!=(const VecRef<Size, Type>& other) const;
+        bool operator!=(const VecRef<Size, Type>& other) const;
 
         // ENDREGION
 
         // REGION ITERATOR
 
-        inline auto begin();
+        auto begin();
 
-        inline auto end();
+        auto end();
 
-        inline auto cbegin() const;
+        auto cbegin() const;
 
-        inline auto cend() const;
+        auto cend() const;
 
-        inline auto rbegin();
+        auto rbegin();
 
-        inline auto rend();
+        auto rend();
 
-        inline auto crbegin() const;
+        auto crbegin() const;
 
-        inline auto crend() const;
+        auto crend() const;
 
         // ENDREGION
 
@@ -496,9 +581,7 @@ namespace rush {
 #endif
 
         // ENDREGION
-
     };
-
 }
 
 #include <rush/vector/vec_impl.h>
