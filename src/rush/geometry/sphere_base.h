4f5d54b0dc0c8e385d50770d3fa33a6fae149a88
Gael Rial <gael.rial.costas@gmail.com>
2024-05-09 15:05:51 +0200

    Add sphere docs.

--- src/rush/geometry/sphere_base.h
+++ src/rush/geometry/sphere_base.h
@@ -8,20 +8,40 @@
 #include <rush/vector/vec.h>
 
 namespace rush {
+    /**
+     * Represents a N-dimensional sphere.
+     * @tparam Dimensions the dimensions of the sphere.
+     * @tparam Type the format of the number used in this sphere.
+     * @tparam VAllocator the allocator used to storage the data.
+     */
     template<size_t Dimensions,
         typename Type,
-        typename VAllocator = rush::StaticAllocator>
+        typename VAllocator = StaticAllocator>
     struct Sphere {
-        using VectorType = rush::Vec<Dimensions, Type, VAllocator>;
+        using VectorType = Vec<Dimensions, Type, VAllocator>;
 
         VectorType center;
         Type radius;
 
+        /**
+         * Creates a new sphere at the origin with radius 0.
+         */
         Sphere();
 
+        /**
+         * Creates a new sphere.
+         * @param center_ the center of the sphere.
+         * @param radius_ the radius.
+         */
         Sphere(VectorType center_, Type radius_);
 
-        template<typename PAllocator = rush::StaticAllocator,
+        /**
+         * Returns the closest point to the given point that is inside this sphere.
+         * @tparam PAllocator the allocator of the given point.
+         * @param point the point to test.
+         * @return the closest point.
+         */
+        template<typename PAllocator = StaticAllocator,
             Algorithm Algorithm = Algorithm()>
         Vec<Dimensions, Type, PAllocator>
         closestPoint(const Vec<Dimensions, Type, PAllocator>& point) const;
